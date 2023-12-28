40f22ecd7f70269e6ec5b0919eef5f0f9cfb253a
19kuz <19kuzminykh78@mail.ru>
2023-12-28 13:28:10 +0800

    ДЗ 3.2

---
+++ src/test/java/pro/sky/java/cource3/ru/hogwarts/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.cource3.ru.hogwarts.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
